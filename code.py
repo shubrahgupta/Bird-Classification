40f095230a205ad7cec3a8b6d9e2f99ee879129a
Odinson <50666757+subrah107@users.noreply.github.com>
2019-12-15 04:28:04 +0530

    Created code file
    Purpose: Creates folders according to the birds' names and transfers the respective csv's into the folders

---
+++ code.py
@@ -0,0 +1,33 @@
+
+
+#import the libraries
+import pandas as pd
+import os
+
+#Extract the text file as csv 
+a=pd.read_csv('/home/shubrah/Downloads/new.txt',sep="\t")
+
+a["Diff"]=a["End Time (s)"]-a["Begin Time (s)"]
+#Created a new column for time difference
+
+b=list(set(a["Notes"]))
+#created a set of all species,no repetition
+
+# define the name of the directory to be created
+path1 = "/home/shubrah/bird/"
+
+# define the access rights
+access_rights = 0o755
+
+#making directories
+for i in range(len(b)):
+    path=path1+str(b[i])
+    os.mkdir(path,access_rights)
+
+#grouping the dataframe according to the bird name code
+c=a.groupby("Notes")
+for i in range(len(b)):
+    d=c.get_group(b[i])
+    path=path1+str(b[i])+"/finally.csv"
+    e=d.to_csv(path)   #exporting the final csv into their respective folders
+    

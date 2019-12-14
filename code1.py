ae572d51c6b54b877fb19c4d7d02310bff766613
Odinson <50666757+subrah107@users.noreply.github.com>
2019-12-15 05:17:25 +0530

    Updated code1.py

--- code1.py
+++ code1.py
@@ -1,31 +1,40 @@
+#import the libraries
 import pandas as pd
 import os
-a=pd.read_csv('/home/shubrah/Downloads/new.txt',sep="\t")
-a["Diff"]=a["End Time (s)"]-a["Begin Time (s)"]
-b=list(set(a["Notes"]))
 
-# define the name of the directory to be created
-path1 = "/home/shubrah/bird/"
+#Extract the text file as csv 
+a=pd.read_csv('<location>/new.txt',sep="\t")
+
+a["Diff"]=a["End Time (s)"]-a["Begin Time (s)"]
+#Created a new column for time difference
+
+b=list(set(a["Notes"]))
+#created a set of all species,no repetition
+
+# define the name and location of the directory to be created
+path1 = "<location>/bird/"
 
 # define the access rights
 access_rights = 0o755
+
+#make bird directory
 try:
     os.mkdir(path1,access_rights)    
 except FileExistsError:
     print("Directory already exists")
 
-
+#making directories
 for i in range(len(b)):
-    path=path1+str(b[i])
+    path=path1+"/"+str(b[i])
     try:
         os.mkdir(path,access_rights)    
     except FileExistsError:
         print("Directory already exists")
         continue
 
+#grouping the dataframe according to the bird name code
 c=a.groupby("Notes")
 for i in range(len(b)):
     d=c.get_group(b[i])
-    path=path1+str(b[i])+"/finally.csv"
-    e=d.to_csv(path)
-    
+    path=path1+"/"+str(b[i])+"/finally.csv"
+    e=d.to_csv(path)  #exporting the final csv into their respective folders

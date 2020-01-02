e56196e889af4cd4a147c7b4904fa53b7535542e
Odinson <50666757+subrah107@users.noreply.github.com>
2020-01-03 03:32:06 +0530

    updated code
    works for a file

--- finalfor1file.py
+++ finalfor1file.py
@@ -1,7 +1,8 @@
 #import the libraries
 import pandas as pd
 import os
-from pydub import AudioSegment
+import uuid
+import subprocess
 
 #Extract the text file as csv 
 a=pd.read_csv('/home/shubrah/Downloads/new1.txt',sep="\t")
@@ -37,23 +38,27 @@ for i in range(len(b)):
 c=a.groupby("Notes")
 for i in range(len(b)):
     d=c.get_group(b[i])
-    path=path1+"/"+str(b[i])+"/finally1.csv"
-    e=d.to_csv(path)  #exporting the final csv into their respective folders
+    x=d.drop(['Selection', 'View', 'Channel', 'Low Freq (Hz)', 'High Freq (Hz)', 'Q1 Freq (Hz)', 'Q3 Freq (Hz)',
+       'Avg Power (dB)', 'Begin Date', 'Delta Time (s)', 'Begin File',
+       'Begin Path', 'Center Freq (Hz)', 'Delta Freq (Hz)'],axis=1)
+    path=path1+"/"+str(b[i])+"/"+str(uuid.uuid4())+".csv"
+    e=x.to_csv(path)  #exporting the final csv into their respective folders
+
 
 
 #audiofile location
 files_path = '/home/shubrah/Downloads/'
-file_name = 'P03929TEA1_20170506_070001'
+file_name = 'P03929TEA1_20170506_070001.wav'
 
 
 # Opening file and extracting segment
 for i in range(len(b)):
     d=c.get_group(b[i])
     for j in range(len(d)):
-        start=d["Begin Time (s)"].values[j]*1000  #starttime
-        end=d["End Time (s)"].values[j]*1000     #endtime
-        song = AudioSegment.from_wav( files_path+file_name+'.wav' ) #extracting song
-        extract = song[start:end]   #cutting the song
-        path=path1+"/"+str(b[i])+"/"    #path for exporting
-        extract.export(path+'extract.wav', format="wav") #saving the file
-
+        start=str(d["Begin Time (s)"].values[j])  #starttime
+        end=str(d["End Time (s)"].values[j])   #endtime
+        path=path1+"/"+str(b[i])+"/"    
+        final=path+str(uuid.uuid4())+str('.wav') #final path for file
+        extracting_path=files_path+file_name  #path for exporting
+        cmd=['ch_wave',extracting_path,'-o',final,'-start',start,'-end',end] #terminal_commands
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
